5b636df4d1a16cf8998c9229f1a88fc3315b856d
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-06 22:18:31 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/alandma/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+// AlAndMa/app/src/main/java/com/example/alandma/ui/theme/Color.kt
+package com.example.alandma.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BlushPink    = Color(0xFFF9C6D3)
+val SoftLavender = Color(0xFFC8B6FF)
+val LightSkyBlue = Color(0xFFB2E0F2)
+val WarmCream    = Color(0xFFFFF9F0)
+val SageGreen    = Color(0xFFA8D5BA)
+val GoldAccent   = Color(0xFFFFD700)
