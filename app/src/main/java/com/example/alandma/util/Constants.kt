a46adae6575bc24fbf854a9843289a6dd07c32dd
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-06 22:18:31 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/alandma/util/Constants.kt
@@ -0,0 +1,7 @@
+// AlAndMa/app/src/main/java/com/example/alandma/util/Constants.kt
+package com.example.alandma.util
+
+object Constants {
+    // Change this to point at your local computer’s server (e.g. "http://192.168.1.100:8080/")
+    const val BASE_URL = "http://YOUR_COMPUTER_IP:8080/"
+}
