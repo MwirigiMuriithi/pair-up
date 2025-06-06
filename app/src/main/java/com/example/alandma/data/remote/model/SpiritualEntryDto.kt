5efc2449f0355caba77b9c138ef2c84bced03c01
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-06 22:18:30 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/alandma/data/remote/model/SpiritualEntryDto.kt
@@ -0,0 +1,9 @@
+// AlAndMa/app/src/main/java/com/example/alandma/data/remote/model/SpiritualEntryDto.kt
+package com.example.alandma.data.remote.model
+
+data class SpiritualEntryDto(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val timestampMillis: Long
+)
