44ec1464863570e9c510e2ec0ebfce06e3d2f262
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-06 22:18:31 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/alandma/ui/theme/Shape.kt
@@ -0,0 +1,12 @@
+// AlAndMa/app/src/main/java/com/example/alandma/ui/theme/Shape.kt
+package com.example.alandma.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val AlAndMaShapes = Shapes(
+    small  = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(12.dp),
+    large  = RoundedCornerShape(16.dp)
+)
