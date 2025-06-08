707c9777e586b26660d258bed623fbade209747f
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-08 21:10:00 +0300

    feat(entity): add default values for groupId, createdBy, and updatedBy in SpiritualEntryEntity

--- app/src/main/java/com/example/alandma/data/local/entity/SpiritualEntryEntity.kt
+++ app/src/main/java/com/example/alandma/data/local/entity/SpiritualEntryEntity.kt
@@ -7,9 +7,10 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "spiritual_entries")
 data class SpiritualEntryEntity(
         @PrimaryKey(autoGenerate = true) val id: Long = 0,
-        val groupId: String,
-        val createdBy: String,
-        val updatedBy: String,
+        // TODO: pass in values once I wire in authentication, DO NOT USE DEFAULTS.
+        val groupId: String = "",
+        val createdBy: String = "",
+        val updatedBy: String = "",
         val title: String, // e.g. “Morning Prayer” or “John 3:16”
         val content: String, // prayer text or reflection
         val timestampMillis: Long // when it was created/added
