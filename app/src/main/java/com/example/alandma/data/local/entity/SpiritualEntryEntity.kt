a09c2e3c4382b894d2ebcd125079f16353a19ea4
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-08 19:56:20 +0300

    Update SpiritualEntryEntity : add groupID, createdBy, updatedBy

--- app/src/main/java/com/example/alandma/data/local/entity/SpiritualEntryEntity.kt
+++ app/src/main/java/com/example/alandma/data/local/entity/SpiritualEntryEntity.kt
@@ -6,8 +6,11 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "spiritual_entries")
 data class SpiritualEntryEntity(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
-    val title: String,          // e.g. “Morning Prayer” or “John 3:16”
-    val content: String,        // prayer text or reflection
-    val timestampMillis: Long   // when it was created/added
+        @PrimaryKey(autoGenerate = true) val id: Long = 0,
+        val groupId: String,
+        val createdBy: String,
+        val updatedBy: String,
+        val title: String, // e.g. “Morning Prayer” or “John 3:16”
+        val content: String, // prayer text or reflection
+        val timestampMillis: Long // when it was created/added
 )
