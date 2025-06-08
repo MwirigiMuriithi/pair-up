b107ed7d88cf48d7ba4e765895a01650b12fcd0f
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-08 19:56:20 +0300

    Update TodayTaskDto: add groupID, createdBy, updatedBy

--- app/src/main/java/com/example/alandma/data/remote/model/TodayTaskDto.kt
+++ app/src/main/java/com/example/alandma/data/remote/model/TodayTaskDto.kt
@@ -2,9 +2,12 @@
 package com.example.alandma.data.remote.model
 
 data class TodayTaskDto(
-    val id: Long,
-    val title: String,
-    val description: String?,
-    val isCompleted: Boolean,
-    val dueDateMillis: Long
+        val id: Long,
+        val groupId: String,
+        val createdBy: String,
+        val updatedBy: String,
+        val title: String,
+        val description: String?,
+        val isCompleted: Boolean,
+        val dueDateMillis: Long
 )
