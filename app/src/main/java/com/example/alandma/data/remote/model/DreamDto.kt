ae6af98a05eca1b34ac52f5d91af1fe9e74f1d84
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-08 19:56:20 +0300

    Modify DreamDto : add groupID, createdBy, updatedBy

--- app/src/main/java/com/example/alandma/data/remote/model/DreamDto.kt
+++ app/src/main/java/com/example/alandma/data/remote/model/DreamDto.kt
@@ -2,9 +2,12 @@
 package com.example.alandma.data.remote.model
 
 data class DreamDto(
-    val id: Long,
-    val title: String,
-    val description: String?,
-    val isAchieved: Boolean,
-    val targetDateMillis: Long
+        val id: Long,
+        val groupId: String,
+        val createdBy: String,
+        val updatedBy: String,
+        val title: String,
+        val description: String?,
+        val isAchieved: Boolean,
+        val targetDateMillis: Long
 )
