b4bbbfccf75daebe284781d84716f717113653fd
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-08 19:56:20 +0300

    Update EventDto : add groupID, createdBy, updatedBy

--- app/src/main/java/com/example/alandma/data/remote/model/EventDto.kt
+++ app/src/main/java/com/example/alandma/data/remote/model/EventDto.kt
@@ -2,9 +2,12 @@
 package com.example.alandma.data.remote.model
 
 data class EventDto(
-    val id: Long,
-    val title: String,
-    val description: String?,
-    val eventDateMillis: Long,
-    val type: String
+        val id: Long,
+        val groupId: String,
+        val createdBy: String,
+        val updatedBy: String,
+        val title: String,
+        val description: String?,
+        val eventDateMillis: Long,
+        val type: String
 )
