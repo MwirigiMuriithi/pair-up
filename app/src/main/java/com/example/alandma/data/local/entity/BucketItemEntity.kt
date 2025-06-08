1cd8730f848963737c84d798db79d41ef711eaa5
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-08 19:56:20 +0300

    Modify BucketItemEntity : add groupID, createdBy, updatedBy

--- app/src/main/java/com/example/alandma/data/local/entity/BucketItemEntity.kt
+++ app/src/main/java/com/example/alandma/data/local/entity/BucketItemEntity.kt
@@ -6,10 +6,12 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "bucket_items")
 data class BucketItemEntity(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
-    val title: String,
-    val category: String, // "Travel", "Food", etc.
-    val isCompleted: Boolean = false,
-    val completedDateMillis: Long? = null,
-    val isFavorite: Boolean = false
+        @PrimaryKey(autoGenerate = true) val id: Long = 0,
+        val groupId: String,
+        val createdBy: String,
+        val updatedBy: String,
+        val category: String, // "Travel", "Food", etc.
+        val isCompleted: Boolean = false,
+        val completedDateMillis: Long? = null,
+        val isFavorite: Boolean = false
 )
