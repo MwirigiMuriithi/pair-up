5100077d4f86ea02424ef5241f5b727218ad5892
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-08 21:10:00 +0300

    feat(entity): add default values for groupId, createdBy, and updatedBy in TodayTaskEntity

--- app/src/main/java/com/example/alandma/data/local/entity/TodayTaskEntity.kt
+++ app/src/main/java/com/example/alandma/data/local/entity/TodayTaskEntity.kt
@@ -7,9 +7,10 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "today_tasks")
 data class TodayTaskEntity(
         @PrimaryKey(autoGenerate = true) val id: Long = 0,
-        val groupId: String,
-        val createdBy: String,
-        val updatedBy: String,
+        // TODO: pass in values once I wire in authentication, DO NOT USE DEFAULTS.
+        val groupId: String = "",
+        val createdBy: String = "",
+        val updatedBy: String = "",
         val title: String,
         val description: String? = null,
         val isCompleted: Boolean = false,
