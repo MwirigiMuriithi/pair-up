2c09debf660531ca5bcf93311175877160f8ba61
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-09 18:03:49 +0300

    Add new Font.kt file for custom fonts

---
+++ app/src/main/java/com/example/alandma/ui/theme/Font.kt
@@ -0,0 +1,13 @@
+// ui/theme/Font.kt
+package com.example.alandma.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.alandma.R
+
+
+// TODO: have custom fonts later, define them in res,font
+val MyCustomFontFamily = FontFamily(
+    //Font(R.font.my_custom,    weight = FontWeight.Normal),
+    //Font(R.font.my_custom_bold, weight = FontWeight.Bold)
+)
